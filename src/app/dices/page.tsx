820870b9d5d88f1844fa0c71c88e4554d1a613a5
Santeri Kangas <Santeri.Kangas@student.lab.fi>
2024-04-14 20:32:06 +0300

    Update project to use shadcn component library

--- src/app/dices/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Dices() {
-  return <h1>This is where my dice app will appear!</h1>;
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,32 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+
 export default function Home() {
-  return <h1> 🎲 Hello world! ⚀ ⚁ ⚂ ⚃ ⚄ ⚅</h1>;
+  const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
+
+  return (
+    <main>
+      <h1> 🎲 Hello world! ⚀ ⚁ ⚂ ⚃ ⚄ ⚅</h1>
+      <div className="grid grid-cols-3 gap-8">
+        {arr.map((i) => (
+          <Card key={i}>
+            <CardHeader>
+              <div>
+                <CardTitle>Title for item {i}</CardTitle>
+                <CardDescription>gkdsgokk</CardDescription>
+              </div>
+            </CardHeader>
+            <CardContent>{i * i}</CardContent>
+            <CardFooter>this is the footer</CardFooter>
+          </Card>
+        ))}
+      </div>
+    </main>
+  );
 }
