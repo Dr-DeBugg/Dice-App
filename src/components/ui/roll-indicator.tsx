5d63898afd51b17cc9deae66a7126586d2969447
Santeri Kangas <Santeri.Kangas@student.lab.fi>
2024-06-15 18:56:49 +0300

    Fix throw button hidden when smaller viewport

--- src/components/ui/roll-indicator.tsx
+++ src/components/ui/roll-indicator.tsx
@@ -5,29 +5,24 @@ import { Dialog, DialogContent } from "../shadcn/dialog";
 import { ThrowDice } from "./throw-dice";
 import { Die } from "./columns";
 
-export const RollIndicator = ({ row }: { row: Die}) => {
+export const RollIndicator = ({ row }: { row: Die }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const closeModal = useCallback(() => {
     setIsModalOpen(false);
   }, []);
-  
+
   return (
     <>
-    <Button
-    variant="outline"
-    className="hidden h-8 w-8 p-0 lg:flex"
-    onClick={() => setIsModalOpen(!isModalOpen)}
-  >
-    <span className="sr-only">Go to first page</span>
-    <HandIcon className="h-4 w-4" />
-  </Button>
-        <Dialog open={isModalOpen} onOpenChange={closeModal}>
-        <DialogContent className="sm:max-w-[425px] p-2" >
+      <Button variant="outline" onClick={() => setIsModalOpen(!isModalOpen)}>
+        <HandIcon className="h-4 w-4" />
+      </Button>
+      <Dialog open={isModalOpen} onOpenChange={closeModal}>
+        <DialogContent className="sm:max-w-[425px] p-2">
           <div id="dice-box" className="responsive-dice-box"></div>
-          <ThrowDice row={row}/>
+          <ThrowDice row={row} />
         </DialogContent>
       </Dialog>
-      </>
+    </>
   );
 };
