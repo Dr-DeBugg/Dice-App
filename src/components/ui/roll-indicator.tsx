3cffa1ec0cb8b510d4d50517df61c98056b91584
Santeri Kangas <Santeri.Kangas@student.lab.fi>
2024-06-30 21:10:21 +0300

    Change switch to button

--- src/components/ui/roll-indicator.tsx
+++ src/components/ui/roll-indicator.tsx
@@ -1,23 +1,20 @@
 import React, { useCallback, useState } from "react";
 import { Button } from "../shadcn/button";
-import { HandIcon } from "@radix-ui/react-icons";
+import { HandIcon, PlusCircledIcon, PlusIcon } from "@radix-ui/react-icons";
 import { Dialog, DialogContent } from "../shadcn/dialog";
 import { ThrowDice } from "./throw-dice";
 import { Die } from "./columns";
-import { Switch } from "../shadcn/switch";
 
 export const RollIndicator = ({ row }: { row: Die }) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
-  const [isSwitchChecked, setIsSwitchChecked] = useState(false);
+  const [addAnother, setAddAnother] = useState(false);
 
-  const handleSwitchChange = (checked: boolean) => {
-    setIsSwitchChecked(checked);
+  const handleSwitchChange = () => {
+    setAddAnother(true);
 
-    if (checked) {
-      setTimeout(() => {
-        setIsSwitchChecked(false);
-      }, 500);
-    }
+    setTimeout(() => {
+      setAddAnother(false);
+    }, 250);
   };
 
   const closeModal = useCallback(() => {
@@ -30,12 +27,14 @@ export const RollIndicator = ({ row }: { row: Die }) => {
         <HandIcon className="h-4 w-4" />
       </Button>
       <Dialog open={isModalOpen} onOpenChange={closeModal}>
-        <DialogContent className="sm:max-w-[425px] p-2">
+        <DialogContent className="sm:max-w-[425px] p-2 top-[55%]">
           <div className="flex justify-center w-full py-2 px-4">
-            <Switch checked={isSwitchChecked} onCheckedChange={handleSwitchChange} />
+            <Button variant="ghost" onClick={handleSwitchChange}>
+              <PlusCircledIcon className="h-7 w-7" />
+            </Button>
           </div>
           <div id="dice-box" className="responsive-dice-box"></div>
-          <ThrowDice row={row} addAnotherDie={isSwitchChecked} />
+          <ThrowDice row={row} addAnotherDie={addAnother} />
         </DialogContent>
       </Dialog>
     </>
