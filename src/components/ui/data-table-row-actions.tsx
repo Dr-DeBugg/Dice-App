4b8e3ddb049d0714d0a07d1ddeca09eddbcb6650
Santeri Kangas <Santeri.Kangas@student.lab.fi>
2024-06-30 18:55:56 +0300

    Fix build

--- src/components/ui/data-table-row-actions.tsx
+++ src/components/ui/data-table-row-actions.tsx
@@ -7,10 +7,8 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/shadcn/dropdown-menu";
-import { useCallback, useState } from "react";
+import { useState } from "react";
 import { Die } from "./columns";
-import { Dialog, DialogContent } from "../shadcn/dialog";
-import { ThrowDice } from "./throw-dice";
 
 type RowActionsProps = {
   row: Die;
@@ -19,40 +17,18 @@ type RowActionsProps = {
 
 export function DataTableRowActions({ row, onDelete }: RowActionsProps) {
   const [open, setOpen] = useState(false);
-  const [isModalOpen, setIsModalOpen] = useState(false);
-
-  const openModal = useCallback(() => {
-    setIsModalOpen(true);
-  }, []);
-
-  const closeModal = useCallback(() => {
-    setIsModalOpen(false);
-  }, []);
-
 
   return (
-    <>
-      <DropdownMenu open={open} onOpenChange={(open) => setOpen(open)}>
-        <DropdownMenuTrigger
-          onPointerDown={(e) => e.preventDefault()}
-          onClick={() => setOpen(!open)}
-        >
-          <span className="sr-only">Open menu</span>
-          <MoreHorizontal className="h-4 w-4" />
-        </DropdownMenuTrigger>
-        <DropdownMenuContent align="end">
-          <DropdownMenuLabel>Actions</DropdownMenuLabel>
-          <DropdownMenuSeparator />
-          <DropdownMenuItem onClick={() => onDelete(row.id)}>Delete</DropdownMenuItem>
-          <DropdownMenuItem onClick={() => openModal()}>Roll</DropdownMenuItem>
-        </DropdownMenuContent>
-      </DropdownMenu>
-      <Dialog open={isModalOpen} onOpenChange={closeModal}>
-        <DialogContent className="sm:max-w-[425px] p-2" >
-          <div id="dice-box" className="responsive-dice-box"></div>
-          <ThrowDice row={row}/>
-        </DialogContent>
-      </Dialog>
-    </>
+    <DropdownMenu open={open} onOpenChange={(open) => setOpen(open)}>
+      <DropdownMenuTrigger onPointerDown={(e) => e.preventDefault()} onClick={() => setOpen(!open)}>
+        <span className="sr-only">Open menu</span>
+        <MoreHorizontal className="h-4 w-4" />
+      </DropdownMenuTrigger>
+      <DropdownMenuContent align="end">
+        <DropdownMenuLabel>Actions</DropdownMenuLabel>
+        <DropdownMenuSeparator />
+        <DropdownMenuItem onClick={() => onDelete(row.id)}>Delete</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
   );
 }
