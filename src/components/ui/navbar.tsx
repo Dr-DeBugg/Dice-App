91976a7255e10e32e3cf820896f22c3818c6fc68
Santeri Kangas <Santeri.Kangas@student.lab.fi>
2024-07-20 15:59:24 +0300

    Add return icon, center heading

--- src/components/ui/navbar.tsx
+++ src/components/ui/navbar.tsx
@@ -1,12 +1,20 @@
+"use client";
 import React from "react";
 import { ModeToggle } from "../shadcn/themeToggle";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
+import { ResetIcon } from "@radix-ui/react-icons";
 
 export default function Navbar() {
+  const pathname = usePathname();
+
   return (
     <nav className="flex flex-col xs:flex-row justify-between items-center py-8">
-      <Link className="h1-style" href="/">
-        🎲 Dice App
+      <Link className="h1-style flex items-center" href="/">
+        <span className="flex-shrink-0">🎲 Dice App</span>
+        <div className="flex items-center ml-2">
+          {pathname === "/" ? null : <ResetIcon className="h-7 w-7 ml-2" />}
+        </div>
       </Link>
       <div className="flex flex-row justify-between items-center w-full xs:w-auto">
         <Link className="h2-style" href="/about">
